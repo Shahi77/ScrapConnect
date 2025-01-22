e3304f270c3ae4f1d8c42744591870d4a9b90ed1
Shahi77 <shahiinfinity02@gmail.com>
2025-01-22 12:15:24 +0530

    db migrations

--- src/prisma/migrations/20250122064010_init/migration.sql
+++ src/prisma/migrations/20250122064010_init/migration.sql
@@ -1,13 +1,14 @@
+-- CreateEnum
+CREATE TYPE "UserType" AS ENUM ('Seller', 'Buyer');
+
 -- CreateTable
-CREATE TABLE "buyer" (
+CREATE TABLE "user" (
+    "type" "UserType" NOT NULL,
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "lat" DOUBLE PRECISION NOT NULL,
-    "lng" DOUBLE PRECISION NOT NULL,
-    "availability" BOOLEAN NOT NULL DEFAULT true,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "buyer_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -19,7 +20,6 @@ CREATE TABLE "request" (
     "pickupType" TEXT NOT NULL,
     "lat" DOUBLE PRECISION NOT NULL,
     "lng" DOUBLE PRECISION NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'Pending',
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -27,7 +27,7 @@ CREATE TABLE "request" (
 );
 
 -- AddForeignKey
-ALTER TABLE "request" ADD CONSTRAINT "request_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "seller"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "request" ADD CONSTRAINT "request_sellerId_fkey" FOREIGN KEY ("sellerId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "request" ADD CONSTRAINT "request_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "buyer"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "request" ADD CONSTRAINT "request_buyerId_fkey" FOREIGN KEY ("buyerId") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
