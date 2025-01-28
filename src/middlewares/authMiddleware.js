128ef549715394530ac0ae0032cc91951e4719df
Shahi77 <shahiinfinity02@gmail.com>
2025-01-28 20:06:39 +0530

    fix

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 
-const verifyToken = (req, res) => {
+const verifyToken = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ error: "Access denied. No token provided." });
