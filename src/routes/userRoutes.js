5934905778a50b72aeb46cb0ad805d58deac6b60
Shahi77 <shahiinfinity02@gmail.com>
2025-01-28 20:07:22 +0530

    update: routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { signup, login, logout } = require("../controllers/userController");
+const { verifyToken } = require("../middlewares/authMiddleware");
 
 const userRouter = Router();
 
 userRouter.post("/signup", signup);
 userRouter.post("/login", login);
-userRouter.post("/logout", logout);
+//userRouter.get("/profile", verifyToken, getAllUsers);
+userRouter.post("/logout", verifyToken, logout);
 
 module.exports = userRouter;
